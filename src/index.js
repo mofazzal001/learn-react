11b92401f2942d25669e3d914572f3c3a4f0cce9
Md Mofazzal Hossain <>
2020-03-20 15:07:34 +0100

    Functional compoents learning

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+import faker from 'faker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail author={ faker.name.firstName() }
+                               timeAgo={ faker.date.recent(10).toString() }
+                               content={ faker.lorem.paragraph(2) }
+                               avatar={ faker.image.avatar() }
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author={ faker.name.firstName() }
+                               timeAgo={ faker.date.recent(10).toString() }
+                               content={ faker.lorem.paragraph(2) }
+                               avatar={ faker.image.avatar() }
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author={ faker.name.firstName() }
+                               timeAgo={ faker.date.recent(10).toString() }
+                               content={ faker.lorem.paragraph(2) }
+                               avatar={ faker.image.avatar() }
+                />
+            </ApprovalCard>
+        </div>
+    );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root'),
+);
