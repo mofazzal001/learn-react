8dcdefbb56995c6e49e310c30111c41a47c5b254
Md Mofazzal Hossain <>
2020-03-21 13:05:16 +0100

    Season application
    * Show different background and message based on user location

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import SeasonDisplay from "./SeasonDisplay";
+import Loader from "./Loader";
+
+class App extends React.Component {
+    state = {lat: null, errorMessage: ''};
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat: position.coords.latitude});
+            },
+            (err) => {
+                this.setState({errorMessage: err.message})
+            },
+        );
+    }
+
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div> Error : {this.state.errorMessage} </div>
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
+        }
+        return <Loader message="Please accept the location request"/>
+    }
+
+    // componentDidUpdate(prevProps, prevState, snapshot) {
+    //     console.log(`component did update`);
+    // }
+    //
+    // componentWillUnmount() {
+    //     console.log(`component will unmount`);
+    // }
+
+    render() {
+        return (
+            <div className={`border red`}>
+                {this.renderContent()}
+            </div>
+        );
+    }
+}
+
+export default App;
